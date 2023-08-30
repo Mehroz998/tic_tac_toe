b54d836d46882d09f00ac9eb81d46cb5d94668dd
Mehroz Ali <mehrozali689@gmail.com>
2023-08-30 19:17:09 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [currentPlayer, setCurrentPlayer] = useState('Player 1');
+  const [winner, setWinner] = useState(null);
+
+  useEffect(() => {
+    const calculatedWinner = calculateWinner(board);
+    if (calculatedWinner) {
+      setWinner(calculatedWinner);
+      setTimeout(() => {
+        alert(`${calculatedWinner === 'X' ? 'Player 1' : 'Player 2'} wins!`);
+        handleRestart();
+      }, 100);
+    }
+  }, [board]);
+
+  const handleClick = (index) => {
+    if (board[index] || winner) {
+      return;
+    }
+
+    const newBoard = board.slice();
+    newBoard[index] = currentPlayer === 'Player 1' ? 'X' : 'O';
+
+    setBoard(newBoard);
+    setCurrentPlayer(currentPlayer === 'Player 1' ? 'Player 2' : 'Player 1');
+  };
+
+  const handleRestart = () => {
+    setBoard(Array(9).fill(null));
+    setCurrentPlayer('Player 1');
+    setWinner(null);
+  };
+
+  const status = winner ? `${winner === 'X' ? 'Player 1' : 'Player 2'} wins!` : `Next player: ${currentPlayer}`;
+
+  const renderSquare = (index) => (
+    <button className="square" onClick={() => handleClick(index)}>
+      {board[index]}
+    </button>
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="game">
+      <div className="game-board">
+        <div className="board-row">
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
+        </div>
+      </div>
+      <div className="game-info">
+        <div>{status}</div>
+        <button onClick={handleRestart} className='Reset' style={{marginTop:"30px"}}>Restart Game</button>
+      </div>
     </div>
   );
-}
+};
+
+const calculateWinner = (squares) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+
+  return null;
+};
 
 export default App;
